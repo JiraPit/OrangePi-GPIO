80083a46c663d35e9fdfca9b66cd48729688efcd
Jira Pit <pitakwongjira@gmail.com>
2025-01-21 14:03:01 +0700

    try diff chiops

--- gpio_opi/src/main.rs
+++ gpio_opi/src/main.rs
@@ -1,11 +1,17 @@
 use gpio_cdev::{Chip, LineRequestFlags};
-// use tokio::time;
+use tokio::time;
 
 #[tokio::main]
 async fn main() {
-    // List available GPIO chips
-    for i in 0..5 {
-        // Check first 5 possible chips
+    for i in 0..10 {
+        let mut chip = Chip::new("/dev/gpiochip0").unwrap();
+        match chip.get_line(118) {
+            Ok(line) => line,
+            Err(e) => {
+                println!("Error: {}", e);
+                continue;
+            }
+        };
         match Chip::new(format!("/dev/gpiochip{}", i)) {
             Ok(chip) => {
                 println!(
@@ -20,4 +26,9 @@ async fn main() {
             }
         }
     }
+
+    // loop {
+    //     println!("Value: {:?}", handle.get_value().unwrap());
+    //     time::sleep(time::Duration::from_secs(1)).await;
+    // }
 }
