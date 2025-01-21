0b74a840fa459a0ecf469f89e0888da316d81ee4
Jira Pit <pitakwongjira@gmail.com>
2025-01-21 13:21:15 +0700

    init

---
+++ gpio_opi/src/main.rs
@@ -0,0 +1,19 @@
+use gpio_cdev::{Chip, LineRequestFlags};
+use tokio::time;
+
+#[tokio::main]
+async fn main() -> ! {
+    // Get the chip and request a line
+    let mut chip = Chip::new("/dev/gpiochip0").unwrap();
+    let handle = chip
+        .get_line(4)
+        .unwrap()
+        .request(LineRequestFlags::INPUT, 0, "read-input")
+        .unwrap();
+
+    // Repeatedly read the input value
+    loop {
+        println!("Value: {:?}", handle.get_value().unwrap());
+        time::sleep(time::Duration::from_secs(1)).await;
+    }
+}
